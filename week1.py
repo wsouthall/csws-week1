85b49e6144bd56330425b036ec9346ed00239961
wsouthall <southallwill99@gmail.com>
2025-01-29 10:33:50 +0000

    lisst

--- week1.py
+++ week1.py
@@ -1,11 +1,15 @@
 for x in range(10):
     print("Hello world" + str(x))
 
-
 names = ["will", "george", "joe"]
 print (names)
 
 print(names[1])
 
 names.sort()
+print(names)
+
+names.append("ben")
+names.reverse()
+
 print(names)
